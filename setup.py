2e0db8dc61248f093dd6fea27d16baf80b968549
zwallen <zdw1229@gmail.com>
2025-07-23 10:05:18 -0400

    fixing plotnine version dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     displays and outputs, transforming and organizing data, performing statistical 
     analyses, and other commonly performed actions in data science projects.""",
   packages=find_packages(),
-  install_requires=['IPython', 'numpy', 'openpyxl', 'pandas', 'plotnine', 'scipy'],
+  install_requires=[
+    'IPython',
+    'numpy',
+    'openpyxl',
+    'pandas',
+    'plotnine>=0.10.1',
+    'scipy',
+  ],
   python_requires='>=3.7',
 )
