6b1bccdec93aabf10f547d6fc9ebd94122580332
zwallen <zdw1229@gmail.com>
2025-07-23 10:13:37 -0400

    adding requirement-dev.txt file, adding more details to setup.py, requiring python >=3.9 to avoid issue with plotnine

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
   description="""A personal collection of convenience functions for styling data 
     displays and outputs, transforming and organizing data, performing statistical 
     analyses, and other commonly performed actions in data science projects.""",
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  license='MIT',
   packages=find_packages(),
   install_requires=[
     'IPython',
@@ -16,5 +19,10 @@ setup(
     'plotnine>=0.10.1',
     'scipy',
   ],
-  python_requires='>=3.7',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.9',
 )
