16e10bfacb1b9ab1bfe381987c3b6a9dc21c00c5
zwallen <49756550+zwallen@users.noreply.github.com>
2025-07-24 19:34:07 +0000

    added tests for required parameters in plotting functions

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -7,7 +7,23 @@ from pycodebox import (
 )
 
 
-def test_stratified_barplot():
+def test_stratified_barplot_required_params():
+  df = pd.DataFrame(
+    {
+      'gender': pd.Categorical(
+        np.random.choice(['Male', 'Female'], size=100, replace=True)
+      ),
+      'treatment': pd.Categorical(
+        np.random.choice(['Treatment', 'Control'], size=100, replace=True)
+      ),
+      'case_id': np.arange(1, 100 + 1),
+    }
+  )
+  p = stratified_barplot(df, 'gender', 'treatment', 'case_id')
+  assert p is not None
+
+
+def test_stratified_barplot_full_params():
   df = pd.DataFrame(
     {
       'gender': pd.Categorical(
@@ -30,7 +46,23 @@ def test_stratified_barplot():
   assert p is not None
 
 
-def test_stratified_violin_boxplot():
+def test_stratified_violin_boxplot_required_params():
+  df = pd.DataFrame(
+    {
+      'survival': pd.concat(
+        [
+          pd.Series(np.random.uniform(50, 100, size=50)),
+          pd.Series(np.random.uniform(0, 50, size=50)),
+        ]
+      ),
+      'treatment': pd.Categorical(['Treatment'] * 50 + ['Control'] * 50),
+    }
+  )
+  p = stratified_violin_boxplot(df, 'survival', 'treatment')
+  assert p is not None
+
+
+def test_stratified_violin_boxplot_full_params():
   df = pd.DataFrame(
     {
       'survival': pd.concat(
@@ -52,7 +84,24 @@ def test_stratified_violin_boxplot():
   assert p is not None
 
 
-def test_stratified_coef_w_ci():
+def test_stratified_coef_w_ci_required_params():
+  df = pd.DataFrame(
+    {
+      'genes': pd.Categorical(
+        ['Gene1'] * 2 + ['Gene2'] * 2 + ['Gene3'] * 2 + ['Gene4'] * 2 + ['Gene5'] * 2
+      ),
+      'treatment': pd.Categorical(['Treatment', 'Control'] * 5),
+      'coef': [0.5, -0.3, 0.2, -0.1, 0.4, -0.2, 0.3, -0.4, 0.1, -0.5],
+      'lower': [0.2, -0.5, 0.1, -0.3, 0.1, -0.4, 0.2, -0.6, 0.05, -0.7],
+      'upper': [0.8, -0.1, 0.3, 0.1, 0.7, 0.0, 0.4, -0.2, 0.15, -0.3],
+      'pvalue': [0.01, 0.05, 0.03, 0.07, 0.02, 0.04, 0.0012, 0.08, 0.09, 0.10],
+    }
+  )
+  p = stratified_coef_w_ci(df, 'genes', 'treatment', 'coef', 'lower', 'upper')
+  assert p is not None
+
+
+def test_stratified_coef_w_ci_full_params():
   df = pd.DataFrame(
     {
       'genes': pd.Categorical(
