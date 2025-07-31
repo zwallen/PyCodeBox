88949ccb985b648140c58b9ba1bbb38a7959328a
zwallen <zdw1229@gmail.com>
2025-07-31 17:19:30 -0400

    removed itertools from install_packages requirement on setup, was causing tests to fail

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
   install_requires=[
     'adjustText',
     'IPython',
-    'itertools',
     'matplotlib',
     'numpy',
     'openpyxl',
@@ -27,5 +26,5 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
   ],
-  python_requires='>=3.11',
+  python_requires='>=3.8',
 )
